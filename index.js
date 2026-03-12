af795d63be6ba94cfca0efe9095c583d0b2fe02e
Pruthuvi Fernando <pruthuvif@mybudget.com.au>
2026-03-12 14:56:25 +0530

    add POST route

--- index.js
+++ index.js
@@ -1,29 +1,58 @@
-const express = require('express')
+const express = require("express")
 const app = express()
 const PORT = 3001
 
-const books = [
+let books = [
   {
-    title: 'The Lord of the Rings',
-    authors: ['J. R. R. Tolkien'],
+    title: "The Lord of the Rings",
+    authors: ["J. R. R. Tolkien"],
     year: 1945,
-    genres: ['classics'],
+    genres: ["classics"],
+    status: "Read",
+    id: "1",
   },
   {
-    title:
-      'The Pragmatic Programmer: Your Journey to Mastery',
-    author: ['David Thomas', 'Andrew Hunt'],
+    title: "The Pragmatic Programmer: Your Journey to Mastery",
+    author: ["David Thomas", "Andrew Hunt"],
     year: 1999,
-    genres: ['Computer Science'],
+    genres: ["Computer Science"],
+    status: "Not started",
+    id: "2",
   },
 ]
 
-app.get('/', (request, response) => {
-  response.send('Hello world!')
+app.use(express.json())
+
+app.get("/", (req, res) => {
+  res.send("Hello world!")
 })
 
-app.get('/api/books', (request, response) => {
-  response.json(books)
+app.get("/api/books", (req, res) => {
+  res.json(books)
+})
+
+app.get("/api/books/:id", (req, res) => {
+  const id = req.params.id
+  const book = books.find((book) => book.id === id)
+
+  if (book) {
+    res.json(book)
+  } else {
+    res.status(404).end()
+  }
+})
+
+app.delete("/api/books/:id", (req, res) => {
+  const id = req.params.id
+  books = books.filter((book) => book.id !== id)
+
+  res.status(204).end()
+})
+
+app.post("/api/books/", (req, res) => {
+  const book = req.body
+  console.log(book)
+  res.json(book)
 })
 
 app.listen(PORT, () => {
